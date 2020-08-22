028459d122612faa55018a490ed5dfedc2ec0200
jzl1 <34795003+jzl1@users.noreply.github.com>
2020-08-22 16:40:23 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python3 hostloc_get_points.py >> /app/app.log
+python3 hostloc_get_points.py
 set i=2
 while true
 do
 let "i=i+1"
-python3 hostloc_get_points.py >> /app/app.log
+python3 hostloc_get_points.py
 sleep 3h
 done
